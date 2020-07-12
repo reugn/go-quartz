2a007cca9a7d3731aed5c534f91d1a2aee6b3f9f
reugn <reugpro@gmail.com>
2020-07-12 19:20:03 +0300

    comment on PrintJob exported methods

--- examples/print_job.go
+++ examples/print_job.go
@@ -6,19 +6,22 @@ import (
 	"github.com/reugn/go-quartz/quartz"
 )
 
-//implements quartz.Job interface
+// PrintJob implements the quartz.Job interface.
 type PrintJob struct {
 	desc string
 }
 
+// Description returns a PrintJob description.
 func (pj PrintJob) Description() string {
 	return pj.desc
 }
 
+// Key returns a PrintJob unique key.
 func (pj PrintJob) Key() int {
 	return quartz.HashCode(pj.Description())
 }
 
+// Execute Called by the Scheduler when a Trigger fires that is associated with the Job.
 func (pj PrintJob) Execute() {
 	fmt.Println("Executing " + pj.Description())
 }
