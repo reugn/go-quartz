cb90061d0abe88ea8f655741fd162b1a401880ff
reugn <reugpro@gmail.com>
2021-12-01 09:45:56 +0200

    update examples

--- examples/print_job.go
+++ examples/print_job.go
@@ -11,17 +11,17 @@ type PrintJob struct {
 	desc string
 }
 
-// Description returns a PrintJob description.
-func (pj PrintJob) Description() string {
+// Description returns the description of the PrintJob.
+func (pj *PrintJob) Description() string {
 	return pj.desc
 }
 
-// Key returns a PrintJob unique key.
-func (pj PrintJob) Key() int {
+// Key returns the unique PrintJob key.
+func (pj *PrintJob) Key() int {
 	return quartz.HashCode(pj.Description())
 }
 
-// Execute Called by the Scheduler when a Trigger fires that is associated with the Job.
-func (pj PrintJob) Execute() {
+// Execute is called by a Scheduler when the Trigger associated with this job fires.
+func (pj *PrintJob) Execute() {
 	fmt.Println("Executing " + pj.Description())
 }
