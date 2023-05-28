46e150c3cdac891ac032e29f8c57822eb888320e
Joaquín Rovira <joaquin.rovira97@gmail.com>
2023-05-28 10:12:33 +0200

    Make gofmt happy

--- quartz/internal/csm/doc.go
+++ quartz/internal/csm/doc.go
@@ -3,7 +3,7 @@
 // instant that fits the expression?
 //
 // The method CronStateMachine.NextTriggerTime() (cron_state_machine.go)
-// computes the answer. The current solution is not proven to be mathematically correct. 
+// computes the answer. The current solution is not proven to be mathematically correct.
 // However, it has been thoroughly validated with multiple complex test cases.
 //
 // A date can be though of as a mixed-radix number (https://en.wikipedia.org/wiki/Mixed_radix).
@@ -14,27 +14,8 @@
 //
 // Second, if no fields are changed by this process, we must perform the smallest possible step forward
 // to find the next valid value. This involves moving forward the least significant field (second),
-// taking care to advance the next significant field when the previous one overflows. This process is 
+// taking care to advance the next significant field when the previous one overflows. This process is
 // implemented in CronStateMachine.next() (fn_next.go).
-//
-// In short, the algorithm can be summarized as follows:
-//
-// ┌─────────────────────────────────┐
-// │ findForward() from current date │
-// └───────────────┬─────────────────┘
-//                 │
-//                 │
-//                 │
-//       ┌─────────▼──────────┐
-//       │ Was Date updated?  ├─────Yes─────────────┐
-//       └─────────┬──────────┘                     │
-//                 │                                │
-//                 No                               │
-//                 │                                │
-// ┌───────────────▼────────────────┐           ┌───▼───┐
-// │ find next() smallest timestep  ├───────────► Done  │
-// └────────────────────────────────┘           └───────┘
-//
 //
 // NOTE: Some precautions must be taken as the "day" value does not have a constant radix. It depends
 // on the month and the year. January always has 30 days, while February 2024 has 29. This is taken into account
