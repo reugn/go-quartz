889d2480b7cee79365cc771f5b934914688931cd
Eugene R <reugpro@gmail.com>
2023-06-02 14:49:02 +0300

    Update quartz/internal/csm/util.go

--- quartz/internal/csm/util.go
+++ quartz/internal/csm/util.go
@@ -1,6 +1,6 @@
 package csm
 
-// Retruns true is the element is included in the slice.
+// Returns true if the element is included in the slice.
 func contained[T comparable](element T, slice []T) bool {
 	for _, e := range slice {
 		if element == e {
