da5bddd92ae724f4ecf984db0acd8ae933b00a68
Eugene R <reugpro@gmail.com>
2024-02-17 14:21:19 +0200

    docs: add package documentation (#108)

--- quartz/doc.go
+++ quartz/doc.go
@@ -1,5 +1,2 @@
-// Package quartz is a simple, zero-dependency scheduling library for Go.
-// Inspired by the Quartz Java scheduler.
-//
-// See README.md for more info.
+// Package quartz contains core components of the scheduling library.
 package quartz
