45b7905672e924effb6280212a71e3708332e151
Eugene R <reugpro@gmail.com>
2024-03-07 09:39:22 +0200

    test: improve code coverage (#116)

--- quartz/job_key_test.go
+++ quartz/job_key_test.go
@@ -11,9 +11,13 @@ import (
 func TestJobKey(t *testing.T) {
 	name := "jobName"
 	jobKey1 := quartz.NewJobKey(name)
+	assert.Equal(t, jobKey1.Name(), name)
+	assert.Equal(t, jobKey1.Group(), quartz.DefaultGroup)
 	assert.Equal(t, jobKey1.String(), fmt.Sprintf("%s::%s", quartz.DefaultGroup, name))
 
 	jobKey2 := quartz.NewJobKeyWithGroup(name, "")
+	assert.Equal(t, jobKey2.Name(), name)
+	assert.Equal(t, jobKey2.Group(), quartz.DefaultGroup)
 	assert.Equal(t, jobKey2.String(), fmt.Sprintf("%s::%s", quartz.DefaultGroup, name))
 
 	if !jobKey1.Equals(jobKey2) {
